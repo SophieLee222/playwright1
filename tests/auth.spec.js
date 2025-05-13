6c391c9f71f95c20b7150855fac272e694110c9e
sofia va <sophia.lily.lee@gmail.com>
2025-05-13 21:40:42 +0300

    screenshots added

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -5,26 +5,32 @@ test.beforeEach(async ({ page }) => {
   await page.goto('https://netology.ru/?modal=sign_in');
 });
 
-test('valid login test', async ({ page }) => {
+test('valid login test', async ({ page }, testInfo) => {
     // Заполнение полей
     await page.getByRole('textbox', { name: 'Email' }).fill(login);
+    await page.screenshot({ path: `screenshots/${testInfo.title}-fillLogin.png` });
     await page.getByRole('textbox', { name: 'Пароль' }).fill(validPassword);
+    await page.screenshot({ path: `screenshots/${testInfo.title}-fillPassword.png` });
     await page.getByTestId('login-submit-btn').click();
 
     // Ожидание перехода
     await expect(page).toHaveURL(/netology\.ru\/profile/);
+    await page.screenshot({ path: `screenshots/${testInfo.title}-displayProfile.png` });
 
     // Проверка контента на странице профиля
     await expect(page.getByTestId('header-top').getByTestId('header-navigatorBtn')).toBeVisible();
   });
 
-  test('invalid login test', async ({ page }) => {
+  test('invalid login test', async ({ page }, testInfo) => {
     // Заполнение полей
     await page.getByRole('textbox', { name: 'Email' }).fill(login);
+    await page.screenshot({ path: `screenshots/${testInfo.title}-fillLogin.png` });
     await page.getByRole('textbox', { name: 'Пароль' }).fill(invalidPassword);
+    await page.screenshot({ path: `screenshots/${testInfo.title}-fillPassword.png` });
     await page.getByTestId('login-submit-btn').click();
 
     // Проверка ошибки
     await expect(page.getByTestId('login-error-hint')).toBeVisible();
     await expect(page.getByTestId('login-error-hint')).toContainText('Вы ввели неправильно логин или пароль');
+    await page.screenshot({ path: `screenshots/${testInfo.title}-displayError.png` });
 });
